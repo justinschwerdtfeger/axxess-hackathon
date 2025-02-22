4e48efa2f22b112f8d1067b03403695aafad5e63
Luke Morrison <162383546+CombatCash1@users.noreply.github.com>
2025-02-22 16:14:54 -0600

    Update +page.svelte

--- src/routes/luke/+page.svelte
+++ src/routes/luke/+page.svelte
@@ -1,2 +1,21 @@
 <p>Luke's route</p>
 <p>test</p>
+<p>a</p>
+
+<script lang="ts">
+    function addPrescription(){
+        alert('Enter your prescription details!');
+        let totalPills = $state(0);
+        let totalDays = $state(0);
+        let dailyPills = $state(0);
+    }
+</script>
+
+
+<btton onclick = {addPrescription}>click me</btton>
+
+<style>
+    button {
+        font-size: 3em;
+    }
+</style>
