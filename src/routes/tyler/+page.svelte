c3e50e5cef0e1ce4664602ebc57cac8f1d515093
Tyler Keonine <tylerkeonine@gmail.com>
2025-02-22 15:35:16 -0600

    Exercise checkbox

--- src/routes/tyler/+page.svelte
+++ src/routes/tyler/+page.svelte
@@ -1,1 +1,10 @@
 <p>Tyler's route</p>
+<script>
+    let cardioCheck = $state(false);
+</script>
+<h1>Welcome to the exercise trainer!</h1>
+<label>
+To get started, select the types of exercises you would like to do:<br>
+Cardio <input type="checkbox" bind:checked={cardioCheck}/>
+{cardioCheck}
+</label>
