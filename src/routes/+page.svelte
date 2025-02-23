7ef4686af3cb0066fecfb3ed151c0bbeae41fbbc
Justin <justinschwerdtfeger245@gmail.com>
2025-02-23 10:10:29 -0600

    update name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,6 @@
 <br>
 <div class="flex flex-col items-center justify-center">
 
-    <h1 class="h1">Welcome to MedStreak!</h1>
+    <h1 class="h1">Welcome to Wellio!</h1>
     <h2 class="h2">Select an option to begin</h2>
 </div>
