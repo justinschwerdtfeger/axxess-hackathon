10005b74ec757d0a8f44ac82f1591df1c74271c9
DashleTan <dashletan@gmail.com>
2025-02-22 22:28:19 -0600

    Update +page.svelte

--- src/routes/dash/+page.svelte
+++ src/routes/dash/+page.svelte
@@ -1,62 +1,103 @@
-<p>Dash's route</p>
 <script lang="ts">
-    import { onMount } from 'svelte';
-    let time = 0;
-    // @ts-ignore
-    let interval;
-    let remainingTime = time;
-    let alarm: HTMLAudioElement;
+	import { onMount } from 'svelte';
+	let time = 0;
+	let tempTime = 0;
+	let interval: ReturnType<typeof setInterval> | null = null;
+	let remainingTime = 0;
+	let alarm: HTMLAudioElement;
+	let checking = false;
 
-onMount(() => {
-    alarm = new Audio("beep.mp3");
-    alarm.onerror = () => {
-        console.error("Failed to load audio file.");
-    };
-});
-    function startTimer(){
-        // @ts-ignore
-        if(!interval && remainingTime > 0) {
-            interval = setInterval(() => {
-                if(remainingTime > 0){
-                    // @ts-ignore
-                    remainingTime -= 1;
-                }else{
-                    // @ts-ignore
-                    clearInterval(interval);
-                    if(alarm){
-                        playAlarm();
-                    }
+	onMount(() => {
+		alarm = new Audio('beep.mp3');
+		alarm.onerror = () => {
+			console.error('Failed to load audio file.');
+		};
+	});
+	function startTimer() {
+    // Ensure remainingTime is set if it's 0 but tempTime is provided
+    if (remainingTime === 0 && tempTime > 0) {
+        remainingTime = tempTime;
+    }
+
+    // Prevent multiple intervals from being created
+	
+    if (!interval && remainingTime > 0) {
+        checking = true;
+        interval = setInterval(() => {
+            if (remainingTime > 0) {
+                remainingTime -= 1;
+            } else {
+				// @ts-ignore
+                clearInterval(interval);
                 interval = null;
+                checking = false;
+                if (alarm) {
+                    playAlarm();
+                }
             }
-        }, 1000)
-            }
-        }
-    // @ts-ignore
-    function resetTimer() {
-        // @ts-ignore
-        stopTimer();
-        remainingTime = time;
-    }
-    // @ts-ignore
-    function updateTime(event){
-        // @ts-ignore
-        time = parseInt(event.target.value) || 0;
-        // @ts-ignore
-        resetTimer();
-    }
-    function stopTimer() {
-    // @ts-ignore
-    clearInterval(interval);
-    interval = null;
-    }
-    function playAlarm() {
-    alarm.play();
-    alert("Time's up!"); // Optional: show an alert
-  }
-    
+        }, 1000);
+    }else{
+		if(checking){
+			remainingTime = tempTime;
+		}
+	}
+}
+	function add() {
+		remainingTime += tempTime;
+
+	}
+	function add15s() {
+		// @ts-ignore
+		remainingTime += 15;
+	}
+	function add15m() {
+		// @ts-ignore
+		remainingTime += 900;
+	}
+	function add1h() {
+		// @ts-ignore
+		remainingTime += 3600;
+	}
+	// @ts-ignore
+	function resetTimer() {
+		// @ts-ignore
+		stopTimer();
+		remainingTime = 0;
+		checking = false;
+	}
+	// @ts-ignore
+	function updateTime(event) {
+		// @ts-ignore
+		time = parseInt(event.target.value) || 0;
+		// @ts-ignore
+		// resetTimer();
+	}
+	function stopTimer() {
+		// @ts-ignore
+		clearInterval(interval);
+		interval = null;
+	}
+	function playAlarm() {
+		alarm.play();
+		alert("Time's up!"); // Optional: show an alert
+	}
+	function formatTime(seconds: number) {
+		const hours = Math.floor(seconds / 3600);
+		const minutes = Math.floor((seconds % 3600) / 60);
+		const sec = seconds % 60;
+		return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
+	}
 </script>
-<h1>Countdown: {remainingTime}s</h1>
-<input type="number" bind:value={time} min="1" on:change={updateTime} />
+
+<p>Dash's route</p>
+<h1>Countdown: {formatTime(remainingTime)}</h1>
+<input  class = "text-black" type="number" bind:value={tempTime} min="1" on:change={updateTime} />
 <button on:click={startTimer}>Start</button>
 <button on:click={stopTimer}>Stop</button>
 <button on:click={resetTimer}>Reset</button>
+<button on:click={add}>Add</button>
+<br />
+
+<button style="margin-left: 210px;" on:click={add15s}>15s</button>
+<button on:click={add15m}>15m</button>
+<button on:click={add1h}>1h</button>
