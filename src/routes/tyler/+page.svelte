af07ab942827ae39056cb21fc696edb61a41b501
Tyler Keonine <tylerkeonine@gmail.com>
2025-02-22 20:01:01 -0600

    setup buttons

--- src/routes/tyler/+page.svelte
+++ src/routes/tyler/+page.svelte
@@ -1,14 +1,23 @@
 <p>Tyler's route</p>
 <script>
-    let cardioCheck = $state(false);
+    let setupStage = $state(0);
     let exerciseTypeNames = $state(["Cardio","Strength Training","Flexibility","Balance"]);
     let exerciseSubNames = $state([["Running","Cycling","Swimming"],["Weightlifting","Bodyweight exercises"],["Yoga","Pilates","Static stretching"],["Standing on one leg","Heel raises","Chair sit-to-stand"]])
     let exerciseTypeEnabled = $state([false,false,false,false]);
-    let exerciseSubEnabled = $state([[false,false,false],[false,false],[false,false,false],[false,false,false]])
+    let exerciseSubEnabled = $state([[true,true,true],[true,true],[true,true,true],[true,true,true]])
+    let minutesPerDay = $state(15);
+
+    function increment() {
+		setupStage++;
+	}
+    function decrement() {
+		setupStage--;
+	}
 
 </script>
 <h1>Welcome to the exercise trainer!</h1>
-<label>
+
+{#if setupStage==0}
 To get started, select the types of exercises you would like to do:<br>
 
 {#each exerciseTypeNames as exerciseType, i}
@@ -20,11 +29,45 @@ To get started, select the types of exercises you would like to do:<br>
     />
 
     {exerciseType}
-    {exerciseTypeEnabled[i]}
+    <!-- {exerciseTypeEnabled[i]} -->
     <br>
 </label>
 
+{#if exerciseTypeEnabled[i]}
+{#each exerciseSubEnabled[i] as exerciseSubType, v}
+    <label>
+        &nbsp;-&nbsp;
+        <input
+            type="checkbox"
+            bind:checked={exerciseSubEnabled[i][v]}
+        />
+        {exerciseSubNames[i][v]}
+        <!-- {exerciseSubEnabled[i][v]} -->
+        <br>
+    </label>
+
+{/each}
+{/if}   
 {/each}
 
+<button onclick={increment} style="background-color:{"#39c41f"}">
+	Next
+</button>
 
-</label>
+{:else if setupStage==1}
+Enter how many minutes you would like to spend each day exercising:<br>
+<input
+type="number"
+style="background-color:{"#cfcfcf"}"
+bind:value={minutesPerDay}
+>
+<br>
+<button onclick={decrement} style="background-color:{"#c51f23"}">
+	Back
+</button>
+
+<button onclick={increment} style="background-color:{"#39c41f"}">
+	Let's go!
+</button>
+
+{/if}
